d1de34da5f7123d67c6d6b08c5f67851be8d97b8
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2022-05-20 22:57:23 -0300

    Implementação de fragmento para o navbar
    Comentei toda implementação de persistência por estar dando erro pra subir a aplicação

---
+++ src/main/java/br/edu/ifpb/pweb2/sorte_io/controller/HomeController.java
@@ -0,0 +1,16 @@
+package br.edu.ifpb.pweb2.sorte_io.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/home")
+    public ModelAndView home(ModelAndView model) {
+        model.setViewName("/home");
+
+        return model;
+    }
+}
--- src/main/java/br/edu/ifpb/pweb2/sorte_io/model/Aposta.java
+++ src/main/java/br/edu/ifpb/pweb2/sorte_io/model/Aposta.java
@@ -2,38 +2,38 @@ package br.edu.ifpb.pweb2.sorte_io.model;
 
 import java.util.List;
 
-import javax.persistence.Column;
+/* import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.Table; */
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "apostas")
+/* @Entity
+@Table(name = "apostas") */
 public class Aposta {
-    @Id
+    /* @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name             = "id_aposta")
+    @Column(name             = "id_aposta") */
     private Integer          id;
 
     private List<Integer>   numSelecionados;
     private int             quantUso;
 
-    @ManyToOne
-    @JoinColumn(name        = "id_apostador")
+    /* @ManyToOne
+    @JoinColumn(name        = "id_apostador") */
     private Apostador       apostador;
 
-    @ManyToOne
-    @JoinColumn(name        = "id_sorteio")
+    /* @ManyToOne
+    @JoinColumn(name        = "id_sorteio") */
     private Sorteio         sorteio;
 }
