bc6d8d95cb19b6c50f5a77ef85f55745ad03faf2
Diego Figueiredo <diegofigmedeiros@gmail.com>
2022-07-04 01:46:47 -0300

    feat: inserir saldo
    Tem um pequeno detalhe: se você colocar valores negativos ele irá decrementar ;P

--- src/main/java/br/edu/ifpb/pweb2/sorte_io/controller/PagamentoController.java
+++ src/main/java/br/edu/ifpb/pweb2/sorte_io/controller/PagamentoController.java
@@ -2,16 +2,55 @@ package br.edu.ifpb.pweb2.sorte_io.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifpb.pweb2.sorte_io.model.Apostador;
+import br.edu.ifpb.pweb2.sorte_io.repository.ApostadoresRepository;
+import br.edu.ifpb.pweb2.sorte_io.services.apostador.imp.ApostadorImpl;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @RequestMapping("/pagamento")
 public class PagamentoController {
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView pagamentos(ModelAndView model) {
-        model.setViewName("/pagamentos/pagamento");
-        return model;
-    }
+
+	@Autowired
+    ApostadoresRepository apostadoresRepository;
+
+	@Autowired
+	ApostadorImpl apostadorService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView pagamentos(ModelAndView model) {
+		model.setViewName("/pagamentos/pagamento");
+		return model;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public ModelAndView save(ModelAndView model, Principal auth, RedirectAttributes flash, @RequestParam(value = "saldo") BigDecimal saldo, @RequestParam(value = "radio") String radio) {
+		
+		Apostador apostador = apostadorService.findByUser(auth.getName());
+		apostador.setGanhos(saldo.add(apostador.getGanhos()));
+
+		apostadoresRepository.save(apostador);
+
+		// System.out.println(saldo);
+		// System.out.println(saldo.getClass().getName());
+		// System.out.println(radio);
+		// System.out.println(saldo.add(apostador.getGanhos()));
+		// System.out.println(apostador.getGanhos());
+		
+		
+		model.setViewName("redirect:/apostas/aposta");
+		flash.addFlashAttribute("sucesso", "Saldo Adicionado!");
+		return model;
+	}
 }
